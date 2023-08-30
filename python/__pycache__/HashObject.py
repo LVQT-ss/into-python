ae927f55f2f66362d5b0e9fdac89e350c9b899c6
LVQT <levietquocthinh@gmail.com>
2023-08-30 13:12:09 +0700

    hash

---
+++ python/__pycache__/HashObject.py
@@ -0,0 +1,14 @@
+# hash and unhash 
+
+n = 69 
+print(n)
+print(n+1 )
+print(n.__add__(1))
+print(n.__sub__(1))
+print(n.__mul__(1))
+print(n.__neg__())
+
+s_1='leviet'
+s_2='quochtinh'
+print(id(s_1))
+print(id(s_2))
