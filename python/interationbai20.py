73b87e894219b456e49965f5114dabb2d5012ce2
LVQT <levietquocthinh@gmail.com>
2023-08-31 16:46:51 +0700

    iteration

---
+++ python/interationbai20.py
@@ -0,0 +1,13 @@
+#interation là để lấy 1 chuỗi hay 1 ký tự torng 1 vòng lặp 
+intera= (x for x in range(3))
+print(intera)
+#print(next(intera))
+#print(next(intera))
+#print(next(intera))
+#print(sum(intera,1))
+print(max(9,5,25,6))
+print(min(9,5,25,6))
+itera =[1,6,7,2]
+print(itera)
+#print(sorted(itera))
+print(sorted(itera, reverse=True))
