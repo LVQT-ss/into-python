21345f07d92633d96f8ebbb5c23260078003adce
LVQT <levietquocthinh@gmail.com>
2023-09-01 12:19:27 +0700

    input

---
+++ python/inputbai22.py
@@ -0,0 +1,22 @@
+name = 'thinh '
+print('le viet quoc' ,name)
+name = 'thor'
+print('day la ',name)
+value = input()
+print('gia tri ma ban da nhap'+value)
+next_value= input('please enter one more value ')
+print('the second value is :' + next_value)
+
+                
+
+name = input('what is your name: ')
+age = int(input('how old are you: '))
+cao = float(input('how tall are you'))
+
+print('hello'+ name)
+print('you are '+str(age)+' years old ')
+print('you are '+str(cao)+' cm tall')
+
+x=eval(input('nhap di '))
+eval('x+2')
+print(eval)
