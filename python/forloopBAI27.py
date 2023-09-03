105c82c53c5555602d5d104c03c2b9bf49cb1502
LVQT <levietquocthinh@gmail.com>
2023-09-03 14:01:13 +0700

    for loop bai 27

--- python/forloopBAI27.py
+++ python/forloopBAI27.py
@@ -52,6 +52,56 @@ print(list(gen))
 
 # BÀI TẬP 
 # CÂU 1 
-lst = [[1,2,3],[4,5,6]]
-for value_lst in range(len(lst)):
-     print(lst(value_lst))
+l = [[1,2,3],[4,5,6]]
+for value_lst in l:
+     value_lst[0]='None'
+
+print(l)
+
+
+# câu 2 
+def generate_spiral_matrix(n):
+    matrix = [[0] * n for _ in range(n)]
+    num = n * n
+    top, bottom, left, right = 0, n - 1, 0, n - 1
+
+    while num >= 1:
+        for i in range(left, right + 1):
+            matrix[top][i] = num
+            num -= 1
+        top += 1
+
+        for i in range(top, bottom + 1):
+            matrix[i][right] = num
+            num -= 1
+        right -= 1
+
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num -= 1
+        bottom -= 1
+
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num -= 1
+        left += 1
+
+    return matrix
+
+def print_spiral_matrix(matrix):
+    n = len(matrix)
+    max_num_width = len(str(n * n - 1))
+
+    for row in matrix:
+        for num in row:
+            num_str = str(num).zfill(max_num_width)
+            print(num_str, end=' ')
+        print()
+
+if __name__ == "__main__":
+    n = int(input("Nhập số cột/hàng của spiral matrix: "))
+    if n <= 0:
+        print("Vui lòng nhập một số nguyên dương.")
+    else:
+        spiral_matrix = generate_spiral_matrix(n)
+        print_spiral_matrix(spiral_matrix)
