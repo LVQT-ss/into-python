8eac52ae5b62d26ed22b41cc7b603cd988ae502b
LVQT <levietquocthinh@gmail.com>
2023-09-05 22:32:11 +0700

    implementation ; jj

---
+++ python/OOP-python/implement.py
@@ -0,0 +1,21 @@
+# tạo lớp kế thừa 
+class sieunhan: 
+    suc_manh = 50 
+    def __init__(self,para_ten,para_vu_khi,para_mausac):
+        self.ten = 'siu nhan '+ para_ten
+        self.vu_khi= para_vu_khi
+        self.mausac= para_mausac 
+
+class sieunhangao(sieunhan):
+    suc_manh = 40
+    def __init__(self,para_ten,para_vu_khi,para_mausac,para_su_thu):
+        #self.ten = 'siu nhan '+ para_ten
+        #self.vu_khi= para_vu_khi
+        #self.mausac= para_mausac 
+        #self.su_thu= para_su_thu
+        # cách ngắn hơn dùng super
+        super().__init__(para_ten,para_vu_khi,para_mausac) 
+        self.su_thu= para_su_thu
+kteam_do = sieunhangao('do ', '  cung ', ' do ','tho beu ')
+print(kteam_do.__dict__)
+print(kteam_do.suc_manh) 
