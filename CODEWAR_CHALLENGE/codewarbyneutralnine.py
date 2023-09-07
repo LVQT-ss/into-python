68c5980a3fb0fa325494ed5f7fe30bdffba96baa
LVQT <levietquocthinh@gmail.com>
2023-09-07 10:47:26 +0700

    codewar practice

---
+++ CODEWAR_CHALLENGE/codewarbyneutralnine.py
@@ -0,0 +1,31 @@
+# exclaimation marks là dấu chấm thang trong bài này thì mình sẽ xóa đi dấu '!' trong string 
+def remove_exclamation_marks(s):
+    return s.replace('!','')
+# remove vowels 
+def disemvowel(string_):
+    vowels = 'aeiou'
+    for v in vowels: 
+        string_ = string_.replace(v,'')
+        string_ = string_.replace(v.upper(),'')
+    return string_
+# reversed the word if the word have the length equal or more than 5 
+# reversed = [::-1]
+def spin_words(sentence):
+    words = sentence.split(" ")
+    words = [word[::-1] if len(word) >=5 else word  for word in words]
+    return " ".join(words)
+
+# find duplicate letter 
+def duplicate_count(text):
+    occured = []
+    found = []
+    counter = 0
+    for letter in text:
+        if letter.lower() not in occured:
+            occured.append(letter.lower())
+        else:
+            if letter.lower() not in found:
+                counter+=1
+                found.append(letter.lower())
+    return counter
+    
