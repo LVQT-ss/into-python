da77072f099f6c4cb351fa7ab1b02528623fff4c
LVQT <levietquocthinh@gmail.com>
2023-09-08 10:02:48 +0700

    LEETCODE

---
+++ CODEWAR_CHALLENGE/8kyu/Century_from_year.py
@@ -0,0 +1,20 @@
+def century(year):
+    # Tính thế kỷ bằng cách chia lấy phần nguyên của năm cho 100
+    thoi_ky = year // 100
+    
+    print(thoi_ky)
+
+    # Kiểm tra nếu năm chia hết cho 100 (năm cuối của một thế kỷ)
+    if year % 100 == 0:
+        # Trả về giá trị thế kỷ
+        return thoi_ky
+    
+    # Trong trường hợp năm không chia hết cho 100
+    else:
+        # Trả về giá trị thế kỷ kế tiếp
+        return thoi_ky + 1
+
+thoi_ky = 1003 // 100
+print(thoi_ky)
+
+# https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/python
