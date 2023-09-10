e8f448979b1f00db076cbf0ca89f5857ba040539
LVQT <levietquocthinh@gmail.com>
2023-09-10 21:19:51 +0700

    7 kyu update

--- CODEWAR_CHALLENGE/7kyu/Triangular_num.py
+++ CODEWAR_CHALLENGE/7kyu/Triangular_num.py
@@ -1,10 +1,20 @@
 #https://www.codewars.com/kata/56d0a591c6c8b466ca00118b/train/python
 
 def is_triangular(t):
+    # Initialize the level variable to 1.
     level = 1
-    while t > 0 : 
-        t -= level 
-        level += 1 
-    return t == 0 
+    
+    # Use a while loop to check if t is greater than 0.
+    while t > 0:
+        # Subtract the current level from t.
+        t -= level
+        
+        # Increment the level by 1 for the next iteration.
+        level += 1
+    
+    # Check if t has become 0 after the loop.
+    # If t is 0, then it's a triangular number, so return True.
+    # Otherwise, return False.
+    return t == 0
 
-print(is_triangular(3))
+print(is_triangular(6))
