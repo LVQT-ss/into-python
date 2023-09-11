61d06b00b0001d90697c84e9fe9425f4d9945fff
LVQT <levietquocthinh@gmail.com>
2023-09-11 20:15:22 +0700

    howkteam practice

---
+++ Howkteam_practive/bai1_input_and_sum.py
@@ -0,0 +1,9 @@
+try:
+    print('Mời bạn nhập số đầu tiên :')
+    num1 = int(input())
+    print('Mời bạn nhập số thứ hai : ')
+    num2 = int(input())
+
+    print('Tổng của hai số là : ', num1 + num2)
+except:
+    print("định dạng đầu vào không hợp lệ")
