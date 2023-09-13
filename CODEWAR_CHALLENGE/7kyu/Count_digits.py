828b13e6133b70c1089f86297f03439573d926b9
LVQT <levietquocthinh@gmail.com>
2023-09-13 13:17:42 +0700

    7 kyu

---
+++ CODEWAR_CHALLENGE/7kyu/Count_digits.py
@@ -0,0 +1,13 @@
+def digits(n):
+    # Chuyển số nguyên thành chuỗi
+    a = str(n)
+    
+    # Đếm số lượng chữ số trong chuỗi
+    count = len(a)
+    
+    return count
+
+# https://www.codewars.com/kata/58fa273ca6d84c158e000052/solutions/python
+
+def digits(n):
+    return len(str(n))
