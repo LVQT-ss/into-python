b1aec90c414d04c7818c87f25e8e24542e6e9209
LVQT <levietquocthinh@gmail.com>
2023-09-30 21:45:14 +0700

    shortest word 7 kyu

---
+++ CODEWAR_CHALLENGE/7kyu/shortest_word.py
@@ -0,0 +1,12 @@
+
+
+s ="bitcoin take over the world maybe who knows perhaps"
+s = s.split(' ')
+#char_counts = [len(item) for item in my_list]
+
+
+char_counts = [len(item) for item in s]
+sort = sorted(char_counts)
+print(sort[0])
+
+
